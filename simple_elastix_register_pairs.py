0a52b109cb1159848a2892a7975a28de59252c7c
rstamatov <55981363+rstamatov@users.noreply.github.com>
2024-10-12 17:37:30 +0300

    Updated
    Updated to fix a discrepancy in time step numbering

--- simple_elastix_register_pairs.py
+++ simple_elastix_register_pairs.py
@@ -33,10 +33,12 @@ def register_pairs():
         os.mkdir("results/results_pairs")
     if not os.path.exists("results/npz"):
         os.mkdir("results/npz")
+    if not os.path.exists("temp"):
+        os.mkdir("temp")
 
     files = list(os.listdir("results/resized/test/images"))
 
-    for img_filename in files:
+    for img_filename in files[:-1]:
 
         t = int(img_filename[-8:-4])
         
@@ -45,26 +47,25 @@ def register_pairs():
         np.savez("results/npz/t" + str(t) + ".npz", img)
 
         # Create the results folders
-        if not os.path.exists("results/results_pairs/backward_" + str(t)):
-            os.mkdir("results/results_pairs/backward_" + str(t))
-        if not os.path.exists("results/results_pairs/forward_" + str(t-1)):
-            os.mkdir("results/results_pairs/forward_" + str(t-1))
+        if not os.path.exists("results/results_pairs/backward_" + str(t+1)):
+            os.mkdir("results/results_pairs/backward_" + str(t+1))
+        if not os.path.exists("results/results_pairs/forward_" + str(t)):
+            os.mkdir("results/results_pairs/forward_" + str(t))
 
        
-        if t > 1000:
-            # Backward registration
-            moving_image_filename = "results/resized/test/images/t"+ str(t) + ".tif"
-            fixed_image_filename = "results/resized/test/images/t" + str(t-1) + ".tif"
-            output_directory = "results/results_pairs/backward_" + str(t)
-            result_image = register_command(moving_image_filename, fixed_image_filename, output_directory)
-            np.savez("results/npz/t" + str(t) + "_backward.npz", result_image)
+        # Backward registration
+        moving_image_filename = "results/resized/test/images/t"+ str(t+1) + ".tif"
+        fixed_image_filename = "results/resized/test/images/t" + str(t) + ".tif"
+        output_directory = "results/results_pairs/backward_" + str(t+1)
+        result_image = register_command(moving_image_filename, fixed_image_filename, output_directory)
+        np.savez("results/npz/t" + str(t+1) + "_backward.npz", result_image)
 
-            # Forward registration
-            moving_image_filename = "results/resized/test/images/t"+ str(t-1) + ".tif"
-            fixed_image_filename = "results/resized/test/images/t" + str(t) + ".tif"
-            output_directory = "results/results_pairs/forward_" + str(t-1)
-            result_image = register_command(moving_image_filename, fixed_image_filename, output_directory)
-            np.savez("results/npz/t" + str(t-1) + "_forward.npz", result_image)
+        # Forward registration
+        moving_image_filename = "results/resized/test/images/t"+ str(t) + ".tif"
+        fixed_image_filename = "results/resized/test/images/t" + str(t+1) + ".tif"
+        output_directory = "results/results_pairs/forward_" + str(t)
+        result_image = register_command(moving_image_filename, fixed_image_filename, output_directory)
+        np.savez("results/npz/t" + str(t) + "_forward.npz", result_image)
 
 ###################################################################################################################
 
